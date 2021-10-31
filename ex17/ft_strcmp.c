c1f69432c7e7abf93671575eecf10b553ae26aa6
uleeeeee <ule@hotmail.fi>
2021-10-31 21:05:16 +0200

    fixed the ex17 to compare the code in ASCII value

--- ex17/ft_strcmp.c
+++ ex17/ft_strcmp.c
@@ -15,14 +15,13 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] == s2[i])
-		{
-			i++;
-		}
-		if (s1[i] != s2[i])
-			return (0);
+		if (s1[i] < s2[i])
+			return (-1);
+		if (s1[i] > s2[i])
+			return (1);
+		i++;
 	}
-	return (1);
+	return (0);
 }
